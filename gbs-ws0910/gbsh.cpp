36483b135e8a15b68723fd0b7741b8c8256c660d
Broever101 <mrassam258@gmail.com>
2020-05-22 05:23:57 +0500

    ls, cd done

--- gbs-ws0910/gbsh.cpp
+++ gbs-ws0910/gbsh.cpp
@@ -5,63 +5,110 @@
 #include <string.h>
 #include <iostream>
 #include <vector>
+#include <errno.h>
+#include <filesystem>
 #include <sstream>
 
+typedef std::vector<std::string> strVec;
+namespace fs = std::filesystem;
+
 std::string getPrompt()
-{	
+{
 	char host[50], cwd[250];
 	gethostname(host, sizeof(host));
-	const char* login = getlogin();
+	const char *login = getlogin();
 	getcwd(cwd, sizeof(cwd));
-	return std::string(login) + "@" + 
-	std::string(login) + " " + std::string(cwd)
-	+ "> ";				
+	return std::string(login) + "@" +
+		   std::string(login) + " " + std::string(cwd) + "> ";
 }
 
-std::string getpwd(){
+std::string getpwd()
+{
 	char pwd[50];
 	getcwd(pwd, sizeof(pwd));
 	return pwd;
 }
 
-void cls(){
-	std::cout<<"\e[1;1H\e[2J"<<'\n';
+void ls(strVec &commands)
+{
+
+	if (commands.size() == 1)
+		commands.push_back(getpwd());
+	if (fs::directory_entry(commands[1]).is_directory())
+	{
+		for (const auto &file : fs::directory_iterator(commands[1]))
+			std::cout << file.path().filename() << "\t";
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << "Not a valid directory.\n";
+	}
 }
 
-std::vector<std::string> tokenize(std::string command){
+void cd(std::string &prompt, strVec &commands)
+{
+	if (commands.size() == 1)
+		commands.push_back("/home");
+	if (chdir(commands[1].c_str()) == 0)
+		prompt = getPrompt();
+	else
+		perror("Error ");
+}
+
+void clear()
+{
+	std::cout << "\e[1;1H\e[2J" << '\n';
+}
+
+std::vector<std::string> tokenize(std::string command)
+{
 	std::stringstream cmd_line(command);
 	std::string token;
 	std::vector<std::string> tokens;
-	while(cmd_line>>token){
+	while (cmd_line >> token)
+	{
 		tokens.push_back(token);
 	}
 	return tokens;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	std::string prompt = getPrompt();
 	std::string command;
-	while(true){
-		std::cout<<prompt;
-		std::getline(std::cin,command);
-		std::vector<std::string> commands = tokenize(command);
-		if (commands[0] == "exit"){
+	while (true)
+	{
+		std::cout << prompt;
+		std::getline(std::cin, command);
+		strVec commands = tokenize(command);
+		if (commands[0] == "exit")
+		{
 			exit(0);
 		}
-		else if (commands[0] == "pwd"){
-			std::cout<<getpwd()<<std::endl;
+		else if (commands[0] == "pwd")
+		{
+			auto path = getpwd();
+			std::cout << path << std::endl;
 		}
-		else if (commands[0] == "clear"){
-			cls();
+		else if (commands[0] == "clear")
+		{
+			clear();
 		}
-		else if (commands[0] == "ls"){
-
+		else if (commands[0] == "ls")
+		{
+			ls(commands);
 		}
-		else{
-			for (auto i:commands)
-				std::cout<<i<<std::endl;
+		else if (commands[0] == "cd")
+		{
+			cd(prompt, commands);
+		}
+		else
+		{
+			for (auto i : commands)
+				std::cout << i << std::endl;
 		}
 	}
- 
-	exit(0); // exit normally	
+
+	exit(0); // exit normally
 }
